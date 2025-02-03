8b159989a660feeb17738d520e448354ecabef9e
Robert1234567-bit <robertoantonioc072@gmail.com>
2025-02-03 16:47:27 -0600

    Add files via upload

--- page.js
+++ page.js
@@ -1,35 +1,39 @@
 "use client";
 import { useState } from "react"; import styles from "./page.module.css";
 
-export default function Home() { const [numero1, setNumero1] = useState(''); const [numero2, setNumero2]
-= useState(''); const [resultado, setResultado] = useState(null);
-
-const sumar = () => {
- const resultadoSuma = parseFloat(numero1) + parseFloat(numero2); setResultado(`Resultado de la suma:
-${resultadoSuma}`);
+export default function Home() { const [numero, setNumero] = useState(1); const [limite, setLimite] =
+useState(10); const [resultado, setResultado] = useState([]);
+ const generarTabla = () => { const nuevaTabla = []; for (let i = 1; i <= limite; i++) {
+ nuevaTabla.push(`${numero} x ${i} = ${numero * i}`);
+ } 
+ setResultado(nuevaTabla);
 };
-const restar = () => {
- const resultadoResta = parseFloat(numero1) - parseFloat(numero2); setResultado(`Resultado de la resta:
-${resultadoResta}`);
-};
- return ( <main className={styles.main}>
- <div className={styles.calculadora}>
- <div className={styles.numeros}>
- <label className={styles.text}>Número 1:</label> <input className={styles.inputnum}
-type="number" value={numero1} onChange={(e) => setNumero1(e.target.value)} />
- </div>
- <div className={styles.numeros}>
- <label className={styles.text} >Número 2:</label> <input
-className={styles.inputnum} type="number" value={numero2} onChange={(e) =>
-setNumero2(e.target.value)} />
- </div>
- <div>
- <button className={styles.button} onClick={sumar}>Sumar</button>
- <button className={styles.button} onClick={restar}>Restar</button>
- </div>
- {resultado && <div
-className={styles.resultado}>{resultado}</div>}
+ return (
+ <main className={styles.main}>
+<div>
+ <h2 className={styles.title2}>Tabla de Multiplicar</h2> <label className={styles.text}>
+ Ingrese un número:
+ <input className={styles.input} type="number" value={numero}
+ onChange={(e) => setNumero(parseInt(e.target.value))}
+ />
+ </label>
+ <br />
+ <label className={styles.text}>
+ Límite de números a mostrar: <input
+className={styles.input} type="number" value={limite}
+ onChange={(e) => setLimite(parseInt(e.target.value))} />
+ </label>
+ <br />
+ <button className={styles.button} onClick={generarTabla}>Generar
+Tabla</button>
+ <hr />
+ <h3>Resultado</h3>
+ <ul className={styles.ul}>
+ {resultado.map((item, index) => (
+ <li className={styles.li} key={index}>{item}</li>
+ ))}
+ </ul>
  </div>
  </main>
 );
-}
+} 
