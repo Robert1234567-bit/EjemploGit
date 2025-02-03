e45b44b7fc997c7d37aa45b745f17cc81857e006
Robert1234567-bit <robertoantonioc072@gmail.com>
2025-02-03 16:55:19 -0600

    Add files via upload

--- page.js
+++ page.js
@@ -1,39 +1,49 @@
-"use client";
-import { useState } from "react"; import styles from "./page.module.css";
-
-export default function Home() { const [numero, setNumero] = useState(1); const [limite, setLimite] =
-useState(10); const [resultado, setResultado] = useState([]);
- const generarTabla = () => { const nuevaTabla = []; for (let i = 1; i <= limite; i++) {
- nuevaTabla.push(`${numero} x ${i} = ${numero * i}`);
- } 
- setResultado(nuevaTabla);
-};
- return (
- <main className={styles.main}>
+import styles from "./page.module.css"; const Equipos = ({ equipos }) => { return (
+  <div className={styles.container__list}>
+   <h2 className={styles.title}>Equipos de Fútbol</h2>
+   {equipos.map((equipo) => (
+   <div key={equipo.id}>
+   <h3 className={styles.nameclub}>{equipo.nombre}</h3>
+   <ul >
+   {equipo.plantilla.map((jugador) => (
+   <li className={styles.container__list} key={jugador.id}>
+   <strong>{jugador.nombre}</strong>
+   <p>Altura: {jugador.Altura}m <br></br> Peso:
+  {jugador.Peso}Kg</p>
+   </li>
+   ))}
+   </ul>
+   </div>
+   ))}
+   </div>
+  );
+  };
+  
+  
+  export default function Home() {
+   // Simula la obtención de datos desde tu JSON const equiposData = [
+   {"id": 1,
+   "nombre": "Real Madrid",
+   "plantilla":[
+   {"id": 1,"nombre":"Eden Hazard","Altura":"1.75","Peso":"74Kg"},
+   {"id": 2,"nombre":"Gonzalo García","Altura":"1.82","Peso":"74Kg"},
+   {"id": 3,"nombre":"Karim Benzema","Altura":"1.85","Peso":"81Kg"}
+   ]},
+   {"id": 2,
+   "nombre": "Barcelona",
+   "plantilla":[
+   {"id": 1,"nombre":"Marc-André ter Stegen","Altura":"1.75","Peso":"74Kg"},
+   {"id": 2,"nombre":"Iñigo Martinez","Altura":"1.82","Peso":"74Kg"},
+   {"id": 3,"nombre":"Gavi","Altura":"1.85","Peso":"81Kg"}
+   ]}
+  }
+   // ... agregar otros equipos
+  ;
+   return ( 
+    <main className={styles.main}>
 <div>
- <h2 className={styles.title2}>Tabla de Multiplicar</h2> <label className={styles.text}>
- Ingrese un número:
- <input className={styles.input} type="number" value={numero}
- onChange={(e) => setNumero(parseInt(e.target.value))}
- />
- </label>
- <br />
- <label className={styles.text}>
- Límite de números a mostrar: <input
-className={styles.input} type="number" value={limite}
- onChange={(e) => setLimite(parseInt(e.target.value))} />
- </label>
- <br />
- <button className={styles.button} onClick={generarTabla}>Generar
-Tabla</button>
- <hr />
- <h3>Resultado</h3>
- <ul className={styles.ul}>
- {resultado.map((item, index) => (
- <li className={styles.li} key={index}>{item}</li>
- ))}
- </ul>
+ <h1>Mi Aplicación de Fútbol</h1>
+ <Equipos equipos={equiposData} />
  </div>
  </main>
 );
-} 
